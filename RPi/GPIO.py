22e7a0019ab7a57d920b1ed96aec2e8fd5281f2d
Michiel-E3 <135598842+Michiel-E3@users.noreply.github.com>
2025-05-12 20:56:11 +0200

    add stepper motor controll file

---
+++ RPi/GPIO.py
@@ -0,0 +1,21 @@
+BCM = 'BCM'
+OUT = 'OUT'
+IN = 'IN'
+LOW = 0
+HIGH = 1
+
+@staticmethod
+def setmode(mode):
+    pass
+
+@staticmethod
+def setup(pin, mode):
+    pass
+
+@staticmethod
+def output(pin, value):
+    pass
+
+@staticmethod
+def cleanup():
+    pass
