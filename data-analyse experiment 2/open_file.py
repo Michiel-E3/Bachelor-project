eaa41aecacccc4aef1ab1d7df4ac801d8a644f36
Michiel-E3 <135598842+Michiel-E3@users.noreply.github.com>
2025-06-10 18:07:19 +0200

    More data-analysis

--- data-analyse experiment 2/open_file.py
+++ data-analyse experiment 2/open_file.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = "data-38.csv"
+filename = "data-3.csv"
 open_file = open(f"data/{filename}", 'r')
 
 t = []
@@ -19,7 +19,7 @@ for line in open_file:
         A2.append(float(data_opgeknipt[3]))
     except:
         if data_opgeknipt[0] == "height of setup:":
-            height = data_opgeknipt[1]
+            height = float(data_opgeknipt[1]) - (13.1 + 42.4)
 open_file.close()
 
 data = np.array([t,A0,A1,A2])
@@ -28,12 +28,13 @@ avg = np.mean(data, axis=1)
 err = np.std(data, axis=1) / np.sqrt(len(data[0]))
 
 print(f"height = {height}")
-for avg_i, err_i in zip(avg, err):
-    print(f"avg = {avg_i} +- {err_i}")
+# for avg_i, err_i in zip(avg, err):
+#     print(f"avg = {avg_i} +- {err_i}")
 
 endtime = 300
 
 plt.plot(t[:endtime], A0[:endtime], color="blue", label="A0")
-plt.plot(t[:endtime], A1[:endtime], color="red", label="A1")
-plt.plot(t[:endtime], A2[:endtime], color="green", label="A2")
+# plt.plot(t[:endtime], A1[:endtime], color="red", label="A1")
+# plt.plot(t[:endtime], A2[:endtime], color="green", label="A2")
+plt.xlim(0,300)
 plt.show()
